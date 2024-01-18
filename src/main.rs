ea731a1c603b98231c77e5926388597dd7db4378
iampushkar <m4manishpushkar@gmail.com>
2024-01-18 09:21:20 +0000

    Scope of variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, Rust!");
+    let x = 15;
+    {
+        let x = x + 2;
+        let y = 51;
+        println!("x = {}, y = {}", x, y); // prints 17, 51
+    }
+    // println!("x = {}, y = {}", x, y); // Error
 }
